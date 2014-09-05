dcca570de598373139b15237151a19103ba5aabe
lukkan99 <lukkan99@gmail.com>
2014-09-05 10:57:21 +0200

    Create numbertiny.cpp

---
+++ numbertiny.cpp
@@ -0,0 +1,25 @@
+//made by lukkan99
+#include <iostream>
+using namespace std;
+int main()
+{
+        int tal;
+cout <<" type a number" << endl;
+cin >> tal;
+system("cls");
+if (tal > 0)
+{
+        tal = tal + 9000;
+        cout <<  tal << " its over 9000!!!!!!!"<< endl;
+        system("Pause");
+        return 0;
+}
+else(tal < 1);
+{
+        tal = tal + 9000;
+        cout <<  tal << endl;
+        system("Pause");
+        return 0;
+}
+ 
+}
