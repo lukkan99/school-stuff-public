8e4608d78db49b691a97164b8fdd0091b5e1fa47
lukkan99 <lukkan99@gmail.com>
2014-09-05 10:38:51 +0200

    Create colorgubben.cpp
    gubben

---
+++ colorgubben.cpp
@@ -0,0 +1,57 @@
+/*warnig it
+has flahing colors
+run on OWN RISK!!!*/
+#include <iostream>
+using namespace std;
+int main()
+{
+                                system("Color 2B");
+                                cout << "\t-  -\n\n\tO  O\n\n\t<.\n\n\t/  \\\n\n     (--------)" << endl;
+                                system("Color 2B");
+                system("Color 3C");
+                                system("Color 3C");
+                system("Color 4D");
+                            system("Color 4D");
+                system("Color 5E");
+                                system("Color 5E");
+                system("Color 6F");
+                                system("Color 6F");
+                system("Color A1");
+                                system("Color A1");
+                system("Color B2");
+                                system("Color B2");
+                system("Color C3");
+                                system("Color C3");
+                system("Color D4");
+                                system("Color D4");
+                system("Color E5");
+                                system("Color E5");
+                system("Color F6");
+                                system("Color F6");
+                                system("Color 2B");
+                                system("Color 2B");
+                system("Color 3C");
+                                system("Color 3C");
+                system("Color 4D");
+                            system("Color 4D");
+                system("Color 5E");
+                                system("Color 5E");
+                system("Color 6F");
+                                system("Color 6F");
+                system("Color A1");
+                                system("Color A1");
+                system("Color B2");
+                                system("Color B2");
+                system("Color C3");
+                                system("Color C3");
+                system("Color D4");
+                                system("Color D4");
+                system("Color E5");
+                                system("Color E5");
+                system("Color F6");
+                                system("Color F6");
+                                system("Color 2B");
+                                cout << "      hello!!!" << endl;
+                                system("pause");
+        return 0;
+}
